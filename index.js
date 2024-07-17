127c521c0f5654cffb2850ca307f00dc1a7bd738
Mbuthiasakara1 <mbuthiaryne@gmail.com>
2024-07-18 00:33:00 +0300

    palindromes lab

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let lowerstr = word.toLowerCase();
+  let newstr = lowerstr.split("").reverse().join("");
+  if (newstr === word) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+let str1 = isPalindrome("robot");
+console.log(str1);
+
+let str2 = isPalindrome("abba");
+console.log(str2);
+
 /* 
-  Add your pseudocode here
+  function
+  write the array methods for the string
+  use return and if else statment
 */
 
 /*
@@ -20,6 +34,18 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mum"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
 }
 
 module.exports = isPalindrome;
